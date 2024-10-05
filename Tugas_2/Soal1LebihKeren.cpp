2f2e5dca9b1dd2244679a4aa4661973e65de4141
Muhamad Yusuf Arohman <140946831+yusufarhman@users.noreply.github.com>
2024-10-05 14:27:43 +0700

    Create Soal1LebihKeren.cpp

---
+++ Tugas_2/Soal1LebihKeren.cpp
@@ -0,0 +1,62 @@
+#include <iostream>
+using namespace std;
+
+const int HARGA_NETFLIX = 39000;
+const int HARGA_DISNEY_PLUS = 45000;
+const int HARGA_HBO_GO = 40000;
+
+void rekomendasiLayananStreaming(int dana) {
+    cout << "Rekomendasi layanan streaming untuk dana Rp. " << dana << ":\n";
+
+
+    if (dana >= HARGA_NETFLIX + HARGA_DISNEY_PLUS + HARGA_HBO_GO) {
+        cout << "- Netflix, Disney+, dan HBO GO\n";
+        cout << "Sisa dana: Rp. " << (dana - (HARGA_NETFLIX + HARGA_DISNEY_PLUS + HARGA_HBO_GO)) << endl;
+    }
+
+
+    if (dana >= HARGA_DISNEY_PLUS + HARGA_HBO_GO) {
+        cout << "- Disney+ dan HBO GO\n";
+        cout << "Sisa dana: Rp. " << (dana - (HARGA_DISNEY_PLUS + HARGA_HBO_GO)) << endl;
+    }
+    if (dana >= HARGA_NETFLIX + HARGA_DISNEY_PLUS) {
+        cout << "- Netflix dan Disney+\n";
+        cout << "Sisa dana: Rp. " << (dana - (HARGA_NETFLIX + HARGA_DISNEY_PLUS)) << endl;
+    }
+    if (dana >= HARGA_NETFLIX + HARGA_HBO_GO) {
+        cout << "- Netflix dan HBO GO\n";
+        cout << "Sisa dana: Rp. " << (dana - (HARGA_NETFLIX + HARGA_HBO_GO)) << endl;
+    }
+
+
+    if (dana < HARGA_NETFLIX + HARGA_HBO_GO) {  
+        if (dana >= HARGA_DISNEY_PLUS) {
+            cout << "- Disney+\n";
+            cout << "Sisa dana: Rp. " << (dana - HARGA_DISNEY_PLUS) << endl;
+        }
+        if (dana >= HARGA_HBO_GO) {
+            cout << "- HBO GO\n";
+            cout << "Sisa dana: Rp. " << (dana - HARGA_HBO_GO) << endl;
+        }
+        if (dana >= HARGA_NETFLIX) {
+            cout << "- Netflix\n";
+            cout << "Sisa dana: Rp. " << (dana - HARGA_NETFLIX) << endl;
+        }
+    }
+
+
+    if (dana < HARGA_NETFLIX && dana < HARGA_HBO_GO && dana < HARGA_DISNEY_PLUS) {
+        cout << "Maaf, dana Anda tidak cukup untuk berlangganan layanan streaming apa pun.\n";
+    }
+}
+
+int main() {
+    int dana;
+    cout << "Masukkan dana yang Anda miliki: Rp. ";
+    cin >> dana;
+
+    cout << endl;
+    rekomendasiLayananStreaming(dana);
+
+    return 0;
+}
